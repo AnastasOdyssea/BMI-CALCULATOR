76f68073897e163d5cfa4e8abc70db343c35a916
Anastas <odhiseaanastas@gmail.com>
2022-11-01 21:46:41 +0000

    js

---
+++ index.js
@@ -0,0 +1,9 @@
+const age = getElementById('age') ;
+const height = getElementById('height');
+const weight = getElementById('weight');
+console.log("age");
+
+function result() {
+    var bmi = 0;
+    bmi = height/(Math.pow(height,2));
+}
